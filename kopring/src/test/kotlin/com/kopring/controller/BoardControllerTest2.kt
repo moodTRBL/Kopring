af67641903fd0a682c47834ea1c279b994bbb5ae
130moodTRBL <sonic6679@naver.com>
2024-09-23 15:02:33 +0900

    수정한 컨트롤러 테스트 (실패)

--- kopring/src/test/kotlin/com/kopring/controller/BoardControllerTest2.kt
+++ kopring/src/test/kotlin/com/kopring/controller/BoardControllerTest2.kt
@@ -1,56 +1,56 @@
-package com.kopring.controller
-
-import com.kopring.dto.response.BoardWriteResponse
-import com.kopring.repository.BoardRepository
-import com.kopring.service.BoardService
-import com.kopring.service.BoardServiceImpl
-import com.kopring.service.FileUploadService
-import com.ninjasquad.springmockk.MockkBean
-import io.kotest.core.spec.style.AnnotationSpec
-import io.mockk.*
-import org.mockito.MockitoAnnotations
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
-import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.boot.test.mock.mockito.MockBean
-import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext
-import org.springframework.http.HttpStatus
-import org.springframework.mock.web.MockMultipartFile
-import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.multipart
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers
-import java.nio.charset.StandardCharsets
-import java.time.LocalDateTime
-
-//@SpringBootTest
-//@AutoConfigureMockMvc
-@WebMvcTest(BoardController::class)
-//@MockBean(JpaMetamodelMappingContext::class)
-class BoardControllerTest2 : AnnotationSpec() {
-    @MockBean
-    private lateinit var boardService: BoardService
-
-    @Autowired
-    private lateinit var mockMvc: MockMvc
-
-    @Test
-    fun `Board를 wrtie하는 POST요청을 보낸다`() {
-        val boardWriteResponse = BoardWriteResponse("title", HttpStatus.OK, LocalDateTime.now())
-        val imageFiles = listOf(
-            MockMultipartFile("file1", "test1.txt", "text/plain", "test1".byteInputStream(StandardCharsets.UTF_8)),
-            MockMultipartFile("file2", "test2.txt", "text/plain", "test2".byteInputStream(StandardCharsets.UTF_8)),
-        )
-        every { boardService.writeBoard(any()) } returns boardWriteResponse
-        mockMvc.multipart("/api/board/write") {
-            param("title", "title")
-            param("content", "content")
-            file("file1", imageFiles[0].bytes);
-            file("file2", imageFiles[1].bytes);
-        }.andExpectAll {
-            status { isOk() }
-            MockMvcResultMatchers.jsonPath("$.title").value(boardWriteResponse.title)
-        }
-        verify(exactly = 1) { boardService.writeBoard(any()) }
-    }
-}
+//package com.kopring.controller
+//
+//import com.kopring.dto.response.BoardWriteResponse
+//import com.kopring.repository.BoardRepository
+//import com.kopring.service.BoardService
+//import com.kopring.service.BoardServiceImpl
+//import com.kopring.service.FileUploadService
+//import com.ninjasquad.springmockk.MockkBean
+//import io.kotest.core.spec.style.AnnotationSpec
+//import io.mockk.*
+//import org.mockito.MockitoAnnotations
+//import org.springframework.beans.factory.annotation.Autowired
+//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+//import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+//import org.springframework.boot.test.context.SpringBootTest
+//import org.springframework.boot.test.mock.mockito.MockBean
+//import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext
+//import org.springframework.http.HttpStatus
+//import org.springframework.mock.web.MockMultipartFile
+//import org.springframework.test.web.servlet.MockMvc
+//import org.springframework.test.web.servlet.multipart
+//import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+//import java.nio.charset.StandardCharsets
+//import java.time.LocalDateTime
+//
+////@SpringBootTest
+////@AutoConfigureMockMvc
+//@WebMvcTest(BoardController::class)
+////@MockBean(JpaMetamodelMappingContext::class)
+//class BoardControllerTest2 : AnnotationSpec() {
+//    @MockBean
+//    private lateinit var boardService: BoardService
+//
+//    @Autowired
+//    private lateinit var mockMvc: MockMvc
+//
+//    @Test
+//    fun `Board를 wrtie하는 POST요청을 보낸다`() {
+//        val boardWriteResponse = BoardWriteResponse("title", HttpStatus.OK, LocalDateTime.now())
+//        val imageFiles = listOf(
+//            MockMultipartFile("file1", "test1.txt", "text/plain", "test1".byteInputStream(StandardCharsets.UTF_8)),
+//            MockMultipartFile("file2", "test2.txt", "text/plain", "test2".byteInputStream(StandardCharsets.UTF_8)),
+//        )
+//        every { boardService.writeBoard(any()) } returns boardWriteResponse
+//        mockMvc.multipart("/api/board/write") {
+//            param("title", "title")
+//            param("content", "content")
+//            file("file1", imageFiles[0].bytes)
+//            file("file2", imageFiles[1].bytes)
+//        }.andExpectAll {
+//            status { isOk() }
+//            MockMvcResultMatchers.jsonPath("$.title").value(boardWriteResponse.title)
+//        }
+//        verify(exactly = 1) { boardService.writeBoard(any()) }
+//    }
+//}
